e84f74a4714ccdbeca391ce018ace9ed5b4d28fd
NitroTech 4K <crtraer@gmail.com>
2022-10-05 15:14:23 -0300

    Created mini API to use later (NOT FINISHED)

--- Program.cs
+++ Program.cs
@@ -5,16 +5,116 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Methods3
+
 {
     internal class Program
     {
+         static int health;
+         static int score;
+         static int shield;
+         static int lives; 
+         static int multiplier;
+
+        static int healthPackValue;
+        static int shieldPackValue;
+
+        static int enemieDamageValue;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Methods 3");
-            
+            score = 0;
+            health = 100;
+            shield = 100;
+            lives = 3;
+
+            ShowHUD();
 
             Console.ReadKey(true);
+
+            playerKilledScreen();
+            
+
+
+
           
         }
+        // This is the quick code for showing the HUD
+
+
+        static void ShowHUD()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("========Your Stats========");
+            Console.WriteLine("=");
+            Console.WriteLine("=Score: " + score + "   Multiplier: " + multiplier + "x");
+            Console.WriteLine("=");
+            Console.WriteLine("=Health: " + health + "   Shield: " + shield);
+            Console.WriteLine("=");
+            Console.WriteLine("=Lives: " + lives);
+            Console.WriteLine("==========================");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.ReadKey(true);
+
+        }
+
+
+       
+        
+        
+        //These are the power ups that affect the HUD in a good way
+
+        static void HealthPack()
+        {
+            health = health + healthPackValue;
+        }
+
+
+        static void ShieldPack()
+        {
+            shield = shield + shieldPackValue;
+        }
+
+
+        
+        
+        
+        //These are the impacts that affect the HUD in a bad way
+
+
+        static void PlayerAttacked()
+        {
+            health = health + enemieDamageValue;
+        }
+
+
+        static void playerKilledScreen()
+        {
+            health = 100;
+            shield = 100;
+            lives = lives - 1;
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("==========================");
+            Console.WriteLine(" You died! " + lives + " lives remain");
+            Console.WriteLine("==========================");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.Beep(400, 300);
+            Console.Beep(380, 1000);
+        }
+
+       
+
+
+        
+          
+
+        
     }
 }
